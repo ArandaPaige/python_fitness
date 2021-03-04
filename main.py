3c1d926018cc425399299a83cafa19be4d66624c
Aranda Paige <ArandaPaige@gmail.com>
2021-03-04 12:50:49 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+
+def main():
+    if BASE_DIR == True:
+        with open(src, 'r', encoding='utf-8') as database, open(src, 'w', encoding='utf-8') as dbedit:
+
+
+
+class User:
+    '''Blah'''
+
+    def __init__(self, *args, **kwargs):
+        self.firstname = firstname
+        self.surname =  surname
+        self.weight = weight
+        self.height = height
+
+    def set_weight(self, weight):
+        self.weight = weight
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def __repr__(self):
+        return f'{self.name}' \
+               f'{self.weight}' \
+               f'{self.height}'
+
+def user_creation()
+
+if __name__ == '__main__':
+    main()
