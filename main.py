1c612e9339496873fafe82cfc83ca0bee8ab7ab1
Aranda Paige <ArandaPaige@gmail.com>
2021-03-04 19:08:21 -0600

    Expanded further on the initial code. A lot of further work needed.

--- main.py
+++ main.py
@@ -1,32 +1,66 @@
-import sys
+import sys, json
+from pathlib import Path
+
+userid = sys.argv[1]
+
+BASE_DIR = pathlib.Path().resolve()
+
+if BASE_DIR.exists('database.txt'):
+    with open('database.txt'), 'r', encoding='utf-8') as dbread:
+        try:
+            if userid in dbread:
+                pass
+        except ValueError:
+            return f''
+
+else:
+    with open('database.txt'), 'w', encoding='utf-8') as dbcreate:
+        try:
+            dbcreate.write()
 
 def main():
-    if BASE_DIR == True:
-        with open(src, 'r', encoding='utf-8') as database, open(src, 'w', encoding='utf-8') as dbedit:
-
-
+    pass
 
 class User:
-    '''Blah'''
+    '''
+    A user is documented with their personal health and fitness statistics.
+    These statistics are collated to generate useful data, like the trajectory of
+    their weight loss/gain.
+    '''
 
     def __init__(self, *args, **kwargs):
         self.firstname = firstname
         self.surname =  surname
-        self.weight = weight
+        self.startingweight = startingweight
+        self.currentweight = weight
         self.height = height
+        self.weight_history = {}
 
     def set_weight(self, weight):
-        self.weight = weight
+        self.currentweight = weight
 
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.surname}'
 
     def __repr__(self):
-        return f'{self.name}' \
+        return f'{self.firstname} {self.surname}' \
                f'{self.weight}' \
                f'{self.height}'
 
-def user_creation()
+    def weight_entry(self, date, weight):
+        '''Assigns a new weight entry to the dictionary with the date as the key'''
+        self.weight_history[date] = weight
+
+    def weight_change(self):
+        '''Averages all of the dictionary's entries and compares to starting weight and current weight'''
+        total = 0
+        for values in self.weight_history.values():
+            total += value
+
+
+
+def user_creation():
+    pass
 
 if __name__ == '__main__':
     main()
